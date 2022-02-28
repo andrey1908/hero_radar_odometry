92ed1dac3d162a92acf149ff3c8a65313b2f6ab8
Keras <andrey.krishtopik@mail.ru>
2022-02-28 08:54:30 +0300

    minor changes

--- docker/start.sh
+++ docker/start.sh
@@ -2,14 +2,14 @@
 
 if [ $# -ne 1 ]; then
     echo "Usage:"
-    echo "./start.sh /path/to/data"
+    echo "./start.sh /path/to/dataset"
     exit -1
 fi
 
 docker run --gpus all --rm -it -d \
     --name hero \
     -v $(realpath $(dirname $0))/../:/home/docker_hero \
-    -v $(realpath $1):/data \
+    -v $(realpath $1):/dataset \
     --shm-size 16G \
     --ipc=host -p 6006:80 \
     hero:latest
