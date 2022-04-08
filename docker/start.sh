e2ea01a37b31030894b01dfddebada74ee6cf2be
andrey1908 <andrey.krishtopik@mail.ru>
2022-04-08 15:02:25 +0300

    minor changes

--- docker/start.sh
+++ docker/start.sh
@@ -2,14 +2,14 @@
 
 if [ $# -ne 1 ]; then
     echo "Usage:"
-    echo "./start.sh /path/to/dataset"
+    echo "./start.sh /path/to/data"
     exit -1
 fi
 
 docker run --gpus all --rm -it -d \
     --name hero \
     -v $(realpath $(dirname $0))/../:/home/docker_hero \
-    -v $(realpath $1):/dataset \
+    -v $(realpath $1):/data \
     --shm-size 16G \
     --net "host" \
     --privileged \
